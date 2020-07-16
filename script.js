80c4b109bbccaa86105a2317401f170df137bcd9
Dan <daniel.murphy.1@gmail.com>
2020-07-15 22:22:06 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const submit = document.getElementById("submit").addEventListener("click", function (event) {
+    event.preventDefault();
+    const tbody = document.getElementById("body");
+    const row = document.createElement("tr");
+    const date = document.getElementById("transaction-date").value;
+    const type = document.getElementById("transaction-type");
+    const selectedType = type.options[type.selectedIndex].text;
+    const location = document.getElementById("location").value;
+    const description = document.getElementById("description").value;
+    const amount = document.getElementById("amount").value;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    const inputs = [date, selectedType, location, description, "$" + amount];
+    console.log(inputs);
+    for (let i = 0; i <= inputs.length-1; i++) {
+      const tdata = document.createElement("td");
+      const text = document.createTextNode(inputs[i]);
+      tdata.appendChild(text);
+      row.appendChild(tdata);
+      row.appendChild(deleteBtn);
+      tbody.appendChild(row);
+    }
+    document.getElementById("form").reset();
+    deleteBtn.addEventListener("click", function () {
+      row.remove();
+    });
+});
